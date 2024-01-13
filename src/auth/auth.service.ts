cd6d2d6fe13412d64768c4d0a9bc4a16f73d7d28
Oğuzhan Söğünmez <oguzhann016s@gmail.com>
2024-01-14 01:16:51 +0300

    first

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{
+    signIn() {
+        return {msg: 'I have signed in'}
+    }
+
+    signUp(){
+        return {msg: 'I have signed up'}
+    }
+}
